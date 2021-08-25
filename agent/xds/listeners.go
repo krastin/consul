07870451be24b6f0353ac3350c6d9a879dfdc514
Paul Banks <pbanks@hashicorp.com>
2021-08-25 17:15:00 +0100

    Remove unused argument to fix lint error

--- agent/xds/listeners.go
+++ agent/xds/listeners.go
@@ -556,7 +556,7 @@ func (s *ResourceGenerator) makeIngressGatewayListeners(address string, cfgSnap 
 		if sdsCfg != nil {
 			// Set up listener TLS from SDS
 			tlsContext = &envoy_tls_v3.DownstreamTlsContext{
-				CommonTlsContext:         makeCommonTLSContextFromSDS(cfgSnap, *sdsCfg),
+				CommonTlsContext:         makeCommonTLSContextFromSDS(*sdsCfg),
 				RequireClientCertificate: &wrappers.BoolValue{Value: false},
 			}
 		} else if cfgSnap.IngressGateway.TLSConfig.Enabled {
@@ -716,7 +716,7 @@ func makeSDSOverrideFilterChains(cfgSnap *proxycfg.ConfigSnapshot,
 			}
 
 			tlsContext := &envoy_tls_v3.DownstreamTlsContext{
-				CommonTlsContext:         makeCommonTLSContextFromSDS(cfgSnap, *svc.TLS.SDS),
+				CommonTlsContext:         makeCommonTLSContextFromSDS(*svc.TLS.SDS),
 				RequireClientCertificate: &wrappers.BoolValue{Value: false},
 			}
 
@@ -1956,7 +1956,7 @@ func makeCommonTLSContextFromLeaf(cfgSnap *proxycfg.ConfigSnapshot, leaf *struct
 	}
 }
 
-func makeCommonTLSContextFromSDS(cfgSnap *proxycfg.ConfigSnapshot, sdsCfg structs.GatewayTLSSDSConfig) *envoy_tls_v3.CommonTlsContext {
+func makeCommonTLSContextFromSDS(sdsCfg structs.GatewayTLSSDSConfig) *envoy_tls_v3.CommonTlsContext {
 	return &envoy_tls_v3.CommonTlsContext{
 		TlsParams: &envoy_tls_v3.TlsParameters{},
 		TlsCertificateSdsSecretConfigs: []*envoy_tls_v3.SdsSecretConfig{
